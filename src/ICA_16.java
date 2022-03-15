e961a2c3dd9d8dfbdd1ee3df6064263848f37f63
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2022-03-15 12:15:43 -0700

    set up 16 and 17 with provided starter code

--- src/ICA_16.java
+++ src/ICA_16.java
@@ -1,2 +1,44 @@
+import java.util.Vector;
+
 public class ICA_16 {
+
+    public static Vector<Vector<Double>> matrix_multiply (Vector<Vector<Double>> inputA,
+                                                          Vector<Vector<Double>> inputB) {
+        // dummy return statement
+        return new Vector<>();
+    }
+
+    public static void main(String[] args) {
+        Vector<Vector<Double>> myDataA = new Vector<>();
+        Vector<Vector<Double>> myDataB = new Vector<>();
+        Vector<Vector<Double>> myDataC = new Vector<>();
+
+        Integer aCols = 8;
+        Integer aRows = 11;
+        Integer bCols = aRows;
+        Integer bRows = aCols;
+
+        for (int i = 0; i < aRows; i++) {
+            Vector<Double> tempRow = new Vector<>();
+            for (int j = 0; j < aCols; j++) {
+                Double temp = (double) (i +j);
+                tempRow.addElement(temp);
+            }
+            myDataA.addElement(tempRow);
+        }
+
+        for (int i = 0; i < bRows; i++) {
+            Vector<Double> tempRow = new Vector<>();
+            for (int j = 0; j < bCols; j++) {
+                Double temp = (double) (i * j);
+                tempRow.addElement(temp);
+            }
+            myDataB.addElement(tempRow);
+        }
+
+        System.out.println(myDataA);
+        System.out.println(myDataB);
+//        myDataC = matrix_multiply(myDataA,myDataB);
+//        System.out.println(myDataC);
+    }
 }
--- src/ICA_17.java
+++ src/ICA_17.java
@@ -1,2 +1,41 @@
+import java.util.Vector;
+
 public class ICA_17 {
+
+    public static Vector<Vector<Double>> square_matrix_multiply(Vector<Vector<Double>> inputA,
+                                                                Vector<Vector<Double>> inputB) {
+        //dummy return statement
+        return new Vector<>();
+    }
+
+    public static void main(String[] args) {
+        Vector<Vector<Double>> myDataA = new Vector<>();
+        Vector<Vector<Double>> myDataB = new Vector<>();
+        Vector<Vector<Double>> myDataC = new Vector<>();
+
+        Integer n = 31;
+
+        for (int i = 0; i < n; i++) {
+            Vector<Double> tempRow = new Vector<>();
+            for (int j = 0; j < n; j++) {
+                Double temp = (double) (i + j);
+                tempRow.addElement(temp);
+            }
+            myDataA.addElement(tempRow);
+        }
+
+        for (int i = 0; i < n; i++) {
+            Vector<Double> tempRow = new Vector<>();
+            for (int j = 0; j < n; j++) {
+                Double temp = (double) (i * j);
+                tempRow.addElement(temp);
+            }
+            myDataB.addElement(tempRow);
+        }
+
+        System.out.println(myDataA);
+        System.out.println(myDataB);
+//        myDataC = square_matrix_multiply(myDataA, myDataB);
+//        System.out.println(myDataC);
+    }
 }
