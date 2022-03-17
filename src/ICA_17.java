7ec5aa470edf2cd10cbb0a806e4ee89e7dad983d
Zecoder <70232378+EzekielJRamirez@users.noreply.github.com>
2022-03-17 11:57:12 -0700

    implemented 17 and added link to matrix calculator

--- src/ICA_17.java
+++ src/ICA_17.java
@@ -2,10 +2,29 @@ import java.util.Vector;
 
 public class ICA_17 {
 
-    public static Vector<Vector<Double>> square_matrix_multiply(Vector<Vector<Double>> inputA,
-                                                                Vector<Vector<Double>> inputB) {
-        //dummy return statement
-        return new Vector<>();
+    public static Vector<Vector<Double>> square_matrix_multiply(
+            Vector<Vector<Double>> inputA,
+            Vector<Vector<Double>> inputB) {
+        // n = inputA.rows
+        Integer n = inputA.size();
+        // let C be a new n x n matrix
+        Vector<Vector<Double>> outC = new Vector<>();
+//        System.out.println(outC + "\n" + outC.size());
+        for (int i = 0;  i < n; i++) {
+            outC.addElement(new Vector<>());
+            for (int j = 0; j < n; j++) {
+//                outC.elementAt(i).elementAt(j).set( (double) 0);
+//                outC.elementAt(i).set(j, 0.0);
+                outC.elementAt(i).add(0.0);
+                for (int k = 0; k < n; k++) {
+                    double a = inputA.elementAt(i).elementAt(k);
+                    double b = inputB.elementAt(k).elementAt(j);
+                    outC.elementAt(i).set(j, outC.elementAt(i).elementAt(j) + a * b);
+                }
+            }
+        }
+//        System.out.println(outC + "\n" + outC.size());
+        return outC;
     }
 
     public static void main(String[] args) {
@@ -13,7 +32,7 @@ public class ICA_17 {
         Vector<Vector<Double>> myDataB = new Vector<>();
         Vector<Vector<Double>> myDataC = new Vector<>();
 
-        Integer n = 31;
+        Integer n = 6;
 
         for (int i = 0; i < n; i++) {
             Vector<Double> tempRow = new Vector<>();
@@ -33,9 +52,11 @@ public class ICA_17 {
             myDataB.addElement(tempRow);
         }
 
-        System.out.println(myDataA);
-        System.out.println(myDataB);
-//        myDataC = square_matrix_multiply(myDataA, myDataB);
-//        System.out.println(myDataC);
+        System.out.println(myDataA + "\nA size: " + myDataA.size());
+        System.out.println();
+        System.out.println(myDataB + "\nB size: " + myDataB.size());
+        System.out.println();
+        myDataC = square_matrix_multiply(myDataA, myDataB);
+        System.out.println(myDataC + "\nC size: " + myDataC.size());
     }
 }
