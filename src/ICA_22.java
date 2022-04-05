4876dd6025d8034ca239148e41574154f0970497
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-04-05 12:44:51 -0700

    Implemented ICA 22

--- src/ICA_22.java
+++ src/ICA_22.java
@@ -1,3 +1,4 @@
+import java.lang.invoke.VolatileCallSite;
 import java.util.Vector;
 
 /**
@@ -10,20 +11,52 @@ public class ICA_22 {
 
     public static void optimal_bst(Vector<Double> p, Vector<Double> q, Integer n) {
         // your code here
-        for (int i = 1; i < n + 1; i++) {
+        Double Maxy = Double.MAX_VALUE;
+        e = new Vector<>();
+        root = new Vector<>();
+        Vector<Vector<Double>> w = new Vector<>();
+        for (int i = 0; i < n; i++) {
+            e.addElement(new Vector<>());
+            root.addElement(new Vector<>());
+            w.addElement(new Vector<>());
+            for (int j = 0; j < n; j++) {
+                e.elementAt(i).addElement( 0.0);
+                root.elementAt(i).addElement(0);
+                w.elementAt(i).addElement(0.0);
+            }
+        }
 
+//        for (int i = 1; i < n + 1; i++) {
+//            e.elementAt(i - 1).set(i - 1, q.elementAt(i - 1));
+//            w.elementAt(i - 1).set(i - 1, q.elementAt(i - 1));
+//        }
+
+        for (int i = 0; i < n; i++) {
+            e.elementAt(i).set(i, q.elementAt(i));
+            w.elementAt(i).set(i, q.elementAt(i));
         }
 
         for (int l = 1; l < n; l++) {
             for (int i = 1; i < n - l + 1; i++) {
                 //
                 int j = i + l - 1;
+                e.elementAt(i - 1).set(j, Maxy);
+                w.elementAt(i - 1).set(j, w.elementAt(i - 1).elementAt(j) +
+                        p.elementAt(j) + q.elementAt(j));
                 for (int r = i; r < j; r++) {
                     //
+                    Double t = e.elementAt(i - 1).elementAt(r - 1)
+                            + e.elementAt(r + 1).elementAt(j)
+                            + w.elementAt(i - 1).elementAt(j);
                     // if (t < e) {}
+                    if (t < e.elementAt(i).elementAt(j)) {
+                        e.elementAt(i - 1).set(j, t);
+                        root.elementAt(i - 1).set(j, r);
+                    }
                 }
             }
         }
+        // return is handled by having the data be global
     }
 
     public static void main(String[] args) {
